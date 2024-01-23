f20bb112532bd773eb2c3c3c7faeaa02dec50a96
Pulkit Dwivedi <pulkit.dwivedi22@spit.ac.in>
2024-01-23 05:45:55 +0530

    config variables stored

---
+++ config/firestore_config.py
@@ -0,0 +1,14 @@
+#firestore db names
+fsTeamDBName = "dev-teams"
+fsClueDBName = "dev-clues"
+fsVolunteerDBName = "dev-volunteers"
+
+#csv file path names
+teamCSV = "csv/firestoreTeamData/test_team_data.csv"
+clueCSV = "csv/firestoreClueData/test_clue_data.csv"
+volunteerCSV = "csv/firestoreVolunteerData/test_volunteer_data.csv"
+
+#json file path names
+teamJSON = "csv/firestoreTeamData/test_team_data.json"
+clueJSON = "csv/firestoreClueData/test_clue_data.json"
+volunteerJSON = "csv/firestoreVolunteerData/test_volunteer_data.json"
