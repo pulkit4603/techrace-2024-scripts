32edc5da58b908781f025bff103766f4328a6c44
Pulkit Dwivedi <pulkit.dwivedi22@spit.ac.in>
2024-01-24 20:49:47 +0530

    named logs

--- config/firestore_config.py
+++ config/firestore_config.py
@@ -9,6 +9,6 @@ clueCSV = "csv/firestoreClueData/test_clue_data.csv"
 volunteerCSV = "csv/firestoreVolunteerData/test_volunteer_data.csv"
 
 #json file path names
-teamJSON = "csv/firestoreTeamData/test_team_data.json"
-clueJSON = "csv/firestoreClueData/test_clue_data.json"
-volunteerJSON = "csv/firestoreVolunteerData/test_volunteer_data.json"
+teamJSON = "csv/firestoreTeamData/logs.json"
+clueJSON = "csv/firestoreClueData/logs.json"
+volunteerJSON = "csv/firestoreVolunteerData/logs.json"
