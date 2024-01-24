d7358fda5d636010e2a520785a5056d92da0123c
Pulkit Dwivedi <pulkit.dwivedi22@spit.ac.in>
2024-01-25 01:28:32 +0530

    minor fix

--- model/firebase.py
+++ model/firebase.py
@@ -7,6 +7,3 @@ firebase_admin.initialize_app(cred, {'databaseURL': 'https://techrace-2024-defau
 realtimeDB = db.reference("/")
 realtimeTeamDB = db.reference("/dev-teams");
 firestoreDB = firestore.client()
-
-# objectname = {"balance": 1000, "currentClueIndex": int("1")}
-# print(realtimeTeamDB.child("007").update(objectname))
